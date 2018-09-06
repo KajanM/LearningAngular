16561c532f14221190596824f1b0f1ae5264a33a
kajan <kajan.14@cse.mrt.ac.lk>
2018-09-06 14:48:52 +0530

    removed unnecessary click me button

--- 02-creating-and-communicating-between-angular-components/ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ 02-creating-and-communicating-between-angular-components/ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -1,30 +1,22 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-thumbnail',
   template: `
   <div class="well hoverwell thumbnail">
-  <h2>{{event.name}}</h2>
-  <div>Date: {{event.date}}</div>
-  <div>Time: {{event.time}}</div>
-  <div>Price: \${{ event.price }}</div>
-  <div>
-    <span>Location: {{event.location.address}}</span>
-    <span>&nbsp;</span>
-    <span>{{event.location.city}}, {{event.location.country}}</span>
-  </div>
-  <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
+    <h2>{{event.name}}</h2>
+    <div>Date: {{event.date}}</div>
+    <div>Time: {{event.time}}</div>
+    <div>Price: \${{ event.price }}</div>
+    <div>
+      <span>Location: {{event.location.address}}</span>
+      <span>&nbsp;</span>
+      <span>{{event.location.city}}, {{event.location.country}}</span>
+    </div>
   </div>
   `
 })
 export class EventThumbnailComponent {
   @Input()
   event: any;
-
-  @Output()
-  eventClick = new EventEmitter();
-
-  handleClickMe() {
-    this.eventClick.emit(this.event.name);
-  }
 }
--- 02-creating-and-communicating-between-angular-components/ng-fundamentals/src/app/events/events-list.component.ts
+++ 02-creating-and-communicating-between-angular-components/ng-fundamentals/src/app/events/events-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   <div>
     <h1>Upcoming Angular Events</h1>
     <hr/>
-    <app-event-thumbnail (eventClick)="handleEventClicked($event)" [event]="event1"></app-event-thumbnail>
+    <app-event-thumbnail [event]="event1"></app-event-thumbnail>
   </div>
 `
 })
@@ -24,8 +24,4 @@ export class EventsListComponent {
       country: 'England'
     }
   };
-
-  handleEventClicked(data) {
-    console.log('received:', data);
-  }
 }
