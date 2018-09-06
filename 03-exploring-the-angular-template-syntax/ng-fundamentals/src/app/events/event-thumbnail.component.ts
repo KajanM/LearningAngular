a81c0a8a33e52734218ea6139d7d9ef8eca1a6b9
kajan <kajan.14@cse.mrt.ac.lk>
2018-09-06 23:41:48 +0530

    fix inconsistent heights for each grid

--- 03-exploring-the-angular-template-syntax/ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ 03-exploring-the-angular-template-syntax/ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -16,6 +16,9 @@ import { Component, Input } from '@angular/core';
   `,
   styles: [
     `
+      .thumbnail {
+        min-height: 210px;
+      }
       .pad-left {
         margin-left: 10px;
       }
